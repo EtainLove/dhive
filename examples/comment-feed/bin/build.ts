2417a5315b3613deec2769efb9b166b14c567b30
etainclub <etainclub@gmail.com>
2020-11-14 16:21:35 +0900

    consoled out sign transaction

--- examples/comment-feed/bin/build.ts
+++ examples/comment-feed/bin/build.ts
@@ -5,15 +5,6 @@ exec('wintersmith build -X') || exit(1);
 exec(
   'uglifyjs build/app.js' +
     ' --source-map "content=inline,url=app.js.map,filename=build/app.js.map"' +
-    ' --compress "dead_code,collapse_vars,reduce_vars,keep_infinity,passes=2"' +
-    ' -o build/app.js'
-);
-
-/*
-exec(
-  'uglifyjs build/app.js' +
-    ' --source-map "content=inline,url=app.js.map,filename=build/app.js.map"' +
     ' --compress "dead_code,collapse_vars,reduce_vars,keep_infinity,drop_console,passes=2"' +
     ' -o build/app.js'
 );
-*/
